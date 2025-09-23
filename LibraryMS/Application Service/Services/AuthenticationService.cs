6dbf6092772839b03eba705c8758a7c23ec32ef5
Mohammadhosein Dehghani <dehghani.fullstack@gmail.com>
2025-09-23 14:21:25 +0330

    Finish Authentication Service

--- LibraryMS/Application Service/Services/AuthenticationService.cs
+++ LibraryMS/Application Service/Services/AuthenticationService.cs
@@ -1,6 +1,46 @@
-﻿namespace LibraryMS.Application_Service.Services;
+﻿using LibraryMS.Domain.Contracts.Service_Contracts;
+using LibraryMS.Domain.Entities;
+using LibraryMS.Domain.Enums;
+using LibraryMS.Domain.Exceptions;
 
-public class AuthenticationService
+namespace LibraryMS.Application_Service.Services;
+
+public class AuthenticationService : IAuthenticationService
 {
-    
+    private readonly IUserService _userService = new UserService();
+
+    public User Register(string firstName, string lastName, string username, string password, string email, UserRoleEnum roll)
+    {
+        var existUser = _userService.GetAll().FirstOrDefault(x => x.Username == username);
+        if (existUser != null)
+        {
+            throw new Exception("Username already exists");
+        }
+
+        var newUser = new User()
+        {
+            FirstName = firstName,
+            LastName = lastName,
+            Username = username,
+            Password = password,
+            Email = email,
+            UserRole = roll
+        };
+        _userService.Add(newUser);
+        return newUser;
+    }
+
+    public User? Login(string username, string password)
+    {
+        var user = _userService.GetAll().FirstOrDefault(x => x.Username == username);
+        if (user == null)
+            throw new Exception("User not found");
+        if (user.Password != password)
+            throw new Exception("username or password is incorrect");
+        
+        if (!user.IsActive)
+            throw new Exception("User is inactive");
+
+        return user;
+    }
 }
--- LibraryMS/Domain/Contracts/Service Contracts/IAuthenticationService.cs
+++ LibraryMS/Domain/Contracts/Service Contracts/IAuthenticationService.cs
@@ -1,9 +1,10 @@
 ﻿using LibraryMS.Domain.Entities;
+using LibraryMS.Domain.Enums;
 
 namespace LibraryMS.Domain.Contracts.Service_Contracts;
 
 public interface IAuthenticationService
 {
-    User Register(string firstName, string lastName, string username, string password, string email);
+    User Register(string firstName, string lastName, string username, string password, string email, UserRoleEnum roll);
     User? Login(string username, string password);
 }
